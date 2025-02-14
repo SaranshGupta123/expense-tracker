9ae976b1d32ec8368147977cc08d222a1b75e735
Saransh gupta <Saranshgupta123456789.sg.sg@gmail.com>
2025-02-14 11:54:16 +0530

    add trransection area . added remove transection ( still working on this) , added unique id for every transections
    from yesterday i just added transection area with red as expense and green as receive and you can delete any transections by clicking on it and i also done chnges when when transections get bigger in size and when try to add expense or receive then when modal is open then tehre is scrolling effect so i removed that also , right now i am working on remove button  because i don't want that when i click on  any transection it will remove it should remove i click on button and  right now i want to store this in localstorage also

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Modal from "./components/Modal";
 import React, { useState } from "react";
+import uniqid from 'uniqid'
 import { GiPayMoney, GiReceiveMoney } from "react-icons/gi";
 import "./App.css";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [modaltype , setModalType] = useState("");
+  const[expenses , setexpenses] = useState([]);
+  const[receives , setreceives] = useState([]);
   const onExpenseClick = ()=>{
     setModalOpen(true);
     setModalType("expense");
@@ -16,12 +19,50 @@ function App() {
     setModalOpen(true);
     setModalType("receive");
   }
+  const Expensehandler = (discription , amount ) => {
+    const oldExpenses = [...expenses];
+    const newExpense = {
+      id : uniqid(),
+      type: "expense",
+      amount : amount,
+      discription : discription,
+    };
 
+    const newExpenses = oldExpenses.concat(newExpense);
+    setexpenses(newExpenses);
+  };
+  const receivehandler = (discription , amount ) => {
+    const oldReceive = [...receives];
+    const newReceive = {
+      id : uniqid(),
+      type: "receive",
+      amount : amount,
+      discription : discription,
+    };
+
+    const newReceives = oldReceive.concat(newReceive);
+    setreceives(newReceives);
+  };
+
+  const RemoveTransection = (type , id) => {
+    if(type === "expense"){
+      const oldExpenses = [...expenses];
+      const newExpenses = oldExpenses.filter((expense)=>expense.id!== id);
+      setexpenses(newExpenses);
+    }
+    if(type === "receive"){
+      const oldReceive = [...receives];
+      const newReceives = oldReceive.filter((receive)=>receive.id!== id);
+      setreceives(newReceives);
+    }
+  }
+  const transactions = [...expenses , ...receives];
+  console.log("Transactions" , transactions);
   return (
     <>
       <div className="App">
         <Header />
-        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} modaltype={modaltype} />
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} modaltype={modaltype} Expensehandler={Expensehandler} receivehandler={receivehandler}/>
         <div className="content">
           <div className="boxes">
             <div className="box-expense" onClick={onExpenseClick}>
@@ -32,6 +73,15 @@ function App() {
               <GiReceiveMoney size={100} color="green" />
               Receive
             </div>
+          </div>
+          <div className="transactions-body">
+            <h1>All Transections</h1>
+            {transactions.map((transaction)=>(
+              <div key={transaction.id} style={{width: "60%", minHeight: "50px" , padding: "10px"  , background: transaction.type === "expense" ? "red" : "green" , display: "flex" , alignItems: "center", marginTop: "20px" , borderRadius: "10px", justifyContent: "space-between" , fontSize:"24px"}} onClick={()=>RemoveTransection(transaction.type , transaction.id)}>
+                <div>{transaction.discription}</div>
+                <div>{transaction.amount}</div>
+              </div>
+            ))}
           </div>
         </div>
         <Footer />
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,18 +2,67 @@ import React, { useEffect, useState } from "react";
 import { RiCloseCircleFill } from "react-icons/ri";
 import "./Modal.css";
 
-const Modal = ({ modalOpen,modaltype, setModalOpen }) => {
+const Modal = ({
+  modalOpen,
+  modaltype,
+  setModalOpen,
+  receivehandler,
+  Expensehandler,
+}) => {
+  const [Discriptions, setDiscriptions] = useState("");
+  const [Amount, setAmount] = useState("");
+  const Buttonclick = () => {
+    if (!Discriptions || !Amount) {
+      return;
+    }
+    if(modaltype==="expense"){
+      Expensehandler(Discriptions , Amount);
+    }
+    if(modaltype==="receive"){
+      receivehandler(Discriptions , Amount);
+    }
+    setModalOpen(false)
+  };
+  useEffect(() => {
+    if (modalOpen) {
+      document.body.style.overflow = "hidden"; // Prevent scrolling
+    } else {
+      document.body.style.overflow = "auto"; // Allow scrolling
+    }
+
+    // Cleanup function to reset overflow when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [modalOpen]);
   return (
     <div
       className={`modal-box ${modalOpen ? "show" : ""}`}
       onClick={() => setModalOpen(false)}
     >
       <div className="modal" onClick={(e) => e.stopPropagation()}>
-      <RiCloseCircleFill size={30} color="black" className="icon-close" onClick={() => setModalOpen(false)}/>
+        <RiCloseCircleFill
+          size={30}
+          color="black"
+          className="icon-close"
+          onClick={() => setModalOpen(false)}
+        />
         <h2>{modaltype === "expense" ? "Add expense" : "Add receive"}</h2>
-        <input type="text" placeholder="Discriptions"/>
-        <input type="text" placeholder="Amount"/>
-        <button>{modaltype === "expense" ? "Add expense" : "Add receive"}</button>
+        <input
+          type="text"
+          placeholder="Discriptions"
+          value={Discriptions}
+          onChange={(e) => setDiscriptions(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          value={Amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={Buttonclick}>
+          {modaltype === "expense" ? "Add expense" : "Add receive"}
+        </button>
       </div>
     </div>
   );
